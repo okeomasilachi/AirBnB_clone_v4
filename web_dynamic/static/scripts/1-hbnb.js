c860ec9441b823d2448085df7493fdf446c0f280
kaydjay <khadijatrasaq@gmail.com>
2024-01-14 22:01:31 +0100

    Refactor amenity checkbox functionality and improve styling

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,8 @@
-{/* <div class="popover">
-    <ul>
-        {% for amenity in amenities %}
-        <li>{{ amenity.name }}</li>
-        {% endfor %}
-    </ul>
-</div> */}
 $(() => {
-    $('.amenities .popover li').addClass("in-line");
-    $('.amenities .popover li').before('<input type="checkbox" />').addClass("in-line");
+    const amen = {};
+    $('.amenities .popover li input').on('change', function (event) {
+        const { id, name } = event.target.dataset;
+        event.target.checked ? amen[name] = id : delete amen[name];
+        $('.amenities h4').text(Object.keys(amen).sort().join(', '));
+    });
 });
