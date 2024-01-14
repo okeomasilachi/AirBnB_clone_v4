278097f83b469e4f255306786e97490edd7aa850
Okeomasilachi <okeomasilachi@gmail.com>
2024-01-15 00:27:07 +0100

    Refactor code to improve performance and readability

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,22 @@
+$(() => {
+    const amen = {};
+    $('.amenities .popover li input').on('change', function (event) {
+        const { id, name } = event.target.dataset;
+        event.target.checked ? amen[name] = id : delete amen[name];
+        $('.amenities h4').text(Object.keys(amen).sort().join(', '));
+    });
+
+    $.ajax({
+        url: 'http://0.0.0.0:5000/api/v1/status',
+        type: 'GET',
+        success: (data) => {
+            if (data['status'] == 'OK') {
+                $('#api_status').addClass('available');
+                $("#api_status").attr("id", "");
+
+            } else {
+                $('header div #api_status').removeClass('available');
+            }
+        }
+    });
+});
